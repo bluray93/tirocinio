c8afd5765d51f3d9a37d36fb94ff988ae73aa77a
bluray93 <bluray219@gmail.com>
2017-11-05 16:50:07 +0100

    start adding terminal shit

--- server.c
+++ server.c
@@ -4,12 +4,12 @@
 #include <unistd.h>
 #include <libwebsockets.h>
 
+#define EXAMPLE_RX_BUFFER_BYTES (32)
+int i=0;
+//static struct lws *web_socket = NULL;
 
 static int callback_http( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len ){
 	switch( reason ){
-		case LWS_CALLBACK_CLIENT_WRITEABLE:
-	    printf("connection established\n");
-	    break;
 		case LWS_CALLBACK_HTTP:
 			lws_serve_http_file( wsi, "index.html", "text/html", NULL, 0 );
 			break;
@@ -21,17 +21,30 @@ static int callback_http( struct lws *wsi, enum lws_callback_reasons reason, voi
 
 static int callback_example( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len ){
 	switch( reason ){
-		case LWS_CALLBACK_ESTABLISHED: // just log message that someone is connecting
+		case LWS_CALLBACK_ESTABLISHED:{ // just log message that someone is connecting
 	    printf("connection established\n");
-	    //client_connections.insert(wsi);
-	    break;
+			lws_callback_on_writable( wsi );
+			//memcpy(web_socket,wsi);
+			//lws_callback_on_writable_all_protocol( lws_get_context( wsi ), lws_get_protocol( wsi ) );
+	    break;}
 		case LWS_CALLBACK_RECEIVE:
-			printf("received some data\n");
 			printf("received data: %s size %d \n", (unsigned char *)(in), (int) len);
+			lws_callback_on_writable( wsi );
 			break;
+		case LWS_CALLBACK_SERVER_WRITEABLE:{
+			unsigned char buf[EXAMPLE_RX_BUFFER_BYTES];
+			memset(buf,0,EXAMPLE_RX_BUFFER_BYTES);
+			strcpy(buf,"Hello! ");
+			char* num=malloc(sizeof(int));
+			sprintf(num,"%d",i);
+			printf("%s\n",num);
+			strcat(buf,num);
+			i++;
+			printf("send p: %s size %d \n", buf, (int)sizeof(buf));
+			lws_write( wsi, buf, EXAMPLE_RX_BUFFER_BYTES, LWS_WRITE_TEXT );
+			break;}
 		case LWS_CALLBACK_CLOSED: { // the funny part
 	    printf("connection closed \n");
-	    //client_connections.erase(wsi);
 	  break;}
 		default:
 			break;
@@ -39,10 +52,7 @@ static int callback_example( struct lws *wsi, enum lws_callback_reasons reason, 
 	return 0;
 }
 
-/* list of supported protocols and callbacks */
-
 static struct lws_protocols protocols[] = {
-    /* first protocol must always be HTTP handler */
     {
       "http-only",    /* name */
       callback_http,  /* callback */
@@ -56,6 +66,11 @@ static struct lws_protocols protocols[] = {
     {NULL, NULL, 0  /* End of list */}
 };
 
+enum protocols{
+	PROTOCOL_EXAMPLE = 0,
+	PROTOCOL_COUNT
+};
+
 int main(void) {
     // server url will be http://localhost:8000
     int port = 8000;
@@ -63,21 +78,17 @@ int main(void) {
     struct lws_context *context;
     // no special options
     int opts = 0;
-
     // create libwebsocket context representing this server
     struct lws_context_creation_info info;
-
     memset(&info, 0, sizeof info);
     info.port = port;
     info.iface = interface;
     info.protocols = protocols;
-    //info.extensions = lws_get_internal_extensions();
     info.ssl_cert_filepath = NULL;
     info.ssl_private_key_filepath = NULL;
     info.gid = -1;
     info.uid = -1;
     info.options = opts;
-
     context = lws_create_context(&info);
     if (context == NULL) {
      fprintf(stderr, "lws init failed\n");
@@ -85,19 +96,21 @@ int main(void) {
     }
 
     printf("starting server...\n");
-
-    // infinite loop, the only option to end this serer is
-    // by sending SIGTERM. (CTRL+C)
     while (1) {
-        lws_service(context, 50);
-        // libwebsocket_service will process all waiting events with their
-        // callback functions and then wait 50 ms.
-        // (this is single threaded webserver and this will keep
-        // our server from generating load while there are not
-        // requests to process)
+			/*if(web_socket==NULL)	{
+				struct lws_client_connect_info ccinfo = {0};
+				ccinfo.context = context;
+				ccinfo.address = "localhost";
+				ccinfo.port = 8000;
+				ccinfo.path = "/";
+				ccinfo.host = lws_canonical_hostname( context );
+				ccinfo.origin = "origin";
+				ccinfo.protocol = protocols[PROTOCOL_EXAMPLE].name;
+				web_socket = lws_client_connect_via_info(&ccinfo);
+			}
+			lws_callback_on_writable( web_socket );*/
+			lws_service(context, 50);
     }
-
     lws_context_destroy(context);
-
     return 0;
 }
